5b256cec92a382dd317778d0c1ad480bfcd66ab1
willejs <williamejsalt@gmail.com>
2015-03-08 13:28:10 +0000

    corrected nat set up commands

--- nat_instance.tf
+++ nat_instance.tf
@@ -3,11 +3,11 @@ resource "aws_instance" "nat_a" {
   ami = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public_a.id}"
-  security_groups = ["${aws_security_group.default-group.id}"]
+  security_groups = ["${aws_security_group.default-group.id}", "${aws_security_group.nat.id}"]
   key_name = "${aws_key_pair.default.key_name}"
   source_dest_check = false
   tags = { 
-    Name = "nat_a"
+    Name = "nat-a"
   }
   connection {
     user = "ubuntu"
@@ -18,8 +18,9 @@ resource "aws_instance" "nat_a" {
     inline = [
       /* Should be in Chef */
       /* "They yellin' Chef" The morning - Raekwon */
-      "sudo iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -j MASQUERADE",
-      "echo 1 | sudo tee /proc/sys/net/ipv4/conf/all/forwarding"
+      "sudo iptables -t nat -C POSTROUTING -o eth0 -s 10.128.0.0/16 -j MASQUERADE || sudo iptables -t nat -A POSTROUTING -o eth0 -s 10.128.0.0/16 -j MASQUERADE",
+      "sudo sysctl -q -w net.ipv4.ip_forward=1 net.ipv4.conf.eth0.send_redirects=0",
+      "sudo sysctl net.ipv4.ip_forward net.ipv4.conf.eth0.send_redirects"
     ]
   }
 }
